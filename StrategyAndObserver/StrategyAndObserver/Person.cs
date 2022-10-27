9d59bdabccb225d4f2c22d130a42ad2b48ed7d6d
vladislav_rupets <vladislav.rupets@gmail.com>
2022-10-27 16:03:52 +0300

    Now we have 3nationalities

--- StrategyAndObserver/StrategyAndObserver/Person.cs
+++ StrategyAndObserver/StrategyAndObserver/Person.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace StrategyAndObserver
 {
-    public abstract class Person
+    /*public class Person
+    {
+        public Person(string name)
+        { }
+        public string Ask()
+        {
+            return "something";
+        }
+        public string Answer(string question)
+        {
+            return "Yes";
+        }
+    }*/
+
+    /*public abstract class Person
     {
         private string name;
         public Person(string name) { this.name = name; }
@@ -35,6 +49,56 @@ namespace StrategyAndObserver
             if (found) return yes;
             else return no;
         }
+    }*/
+    public abstract class Person
+    {
+        private string name;
+        protected string[] vocabulary;
+        protected string yes;
+        protected string no;
+        public Person(string name) { this.name = name; }
+        public string Ask() { return "something"; }
+        public string Answer(string question)
+        {
+            string[] words = question.Split(' ');
+            bool found = false;
+            foreach (string qWord in words)
+            {
+                foreach (string vWord in vocabulary)
+                {
+                    if (qWord == vWord)
+                    { found = true; break; }
+                }
+                if (found) break;
+            }
+            if (found) return yes;
+            else return no;
+        }
     }
+    public class EnglishPerson : Person
+    {
+        public EnglishPerson(string name) : base(name)
+        {
+            vocabulary = new string[] { "table", "apple", "tree" };
+            yes = "yes"; no = "no";
+        }
+    }
+    public class UkrainianPerson : Person
+    {
+        public UkrainianPerson(string name) : base(name)
+        {
+            vocabulary = new string[] { "стіл", "яблуко", "дерево" };
+            yes = "так"; no = "ні";
+        }
+    }
+    public class GermanPerson : Person
+    {
+        public GermanPerson(string name) : base(name)
+        {
+            vocabulary = new string[] { "Tisch", "Apfel", "Baum" };
+            yes = "ja"; no = "nein";
+        }
+    }
+
 
 }
--- StrategyAndObserver/TestProject1/UnitTest1.cs
+++ StrategyAndObserver/TestProject1/UnitTest1.cs
@@ -21,12 +21,22 @@ namespace TestProject1
             Assert.AreEqual("Yes", p3.Answer(question));
 
         }*/
-        [Test]
+        /*[Test]
         public void TestMethod2()
         {
             Person p = new EnglishPerson("A");
             Assert.AreEqual("yes", p.Answer("peace apple nose"));
             Assert.AreEqual("no", p.Answer("milk coffee beer"));
+        }*/
+        [Test]
+        public void TestMethod3() //there are different nationalities
+        {
+            Person p1 = new EnglishPerson("A");
+            Person p2 = new UkrainianPerson("B");
+            Person p3 = new GermanPerson("A");
+            Assert.AreEqual("yes", p1.Answer("peace apple nose"));
+            Assert.AreEqual("í³", p2.Answer("peace apple nose"));
+            Assert.AreEqual("nein", p3.Answer("peace apple nose"));
         }
 
     }
